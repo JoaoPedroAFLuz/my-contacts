fa6587c3014fbe81108c7b4220030e9bcabed2b7
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-10-26 17:45:17 -0300

    feat: create error handler

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-console */
 const express = require('express');
-const router = require('./Routes');
+require('express-async-errors');
+const routes = require('./Routes');
 
 const app = express();
 app.use(express.json());
-app.use(router);
+app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('##### Error Handler #####');
+  console.error(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server startd at http://localhost:3000'));
