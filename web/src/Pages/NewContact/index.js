bfac8d0f28b29f7c866f7afd9983fa4df52ceb40
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-01 18:05:39 -0300

    feat: create input and select components

--- web/src/Pages/NewContact/index.js
+++ web/src/Pages/NewContact/index.js
@@ -1,7 +1,17 @@
+import { Input } from '../../Components/Input';
 import { PageHeader } from '../../Components/PageHeader/inex';
+import { Select } from '../../Components/Select';
 
 export function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option>Instagram</option>
+        <option>Trabalho</option>
+        <option>Faculdade</option>
+      </Select>
+    </>
   );
 }
