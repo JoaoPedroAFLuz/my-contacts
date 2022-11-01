2b895358352cdc774099c9cc9eba56869303b123
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-01 19:56:07 -0300

    feat: add danger status in form group and input

--- web/src/Components/ContactForm/index.js
+++ web/src/Components/ContactForm/index.js
@@ -15,8 +15,8 @@ export function ContactForm({ buttonLabel }) {
         <Input placeholder="Nome" name="name" />
       </FormGroup>
 
-      <FormGroup>
-        <Input placeholder="E-mail" />
+      <FormGroup error="O formato do e-mail é inválido">
+        <Input placeholder="E-mail" error />
       </FormGroup>
 
       <FormGroup>
--- web/src/Components/FormGroup/sytles.js
+++ web/src/Components/FormGroup/sytles.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  small {
+    color: ${({ theme }) => theme.colors.danger.main};
+    font-size: 12px;
+    display: block;
+    margin-top: 8px;
+  }
+
   & + & {
     margin-top: 16px;
   }
