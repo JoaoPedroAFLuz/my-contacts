493d02fbfbdeb3360c054b33858e7784b3bcdf1c
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-04 23:15:33 -0300

    fix: web - toast props

--- web/src/Components/Toast/ToastContainer/index.js
+++ web/src/Components/Toast/ToastContainer/index.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
+
 import { ToastMessage } from '../ToastMessage';
 
 import { Container } from './styles';
 
 export function ToastContainer() {
+  const [message] = useState([
+    { id: Math.random(), type: 'default', text: 'Default text' },
+    { id: Math.random(), type: 'error', text: 'Danger text' },
+    { id: Math.random(), type: 'success', text: 'Success text' },
+  ]);
+
   return (
     <Container>
-      <ToastMessage message="Default toast" />
-      <ToastMessage message="Error toast" type="error" />
-      <ToastMessage message="Success toast" type="success" />
+      {message.map(({ id, type, text }) => (
+        <ToastMessage key={id} type={type} text={text} />
+      ))}
     </Container>
   );
 }
--- web/src/Components/Toast/ToastMessage/index.js
+++ web/src/Components/Toast/ToastMessage/index.js
@@ -4,18 +4,18 @@ import { Container } from './styles';
 import checkCircleIcon from '../../../Assets/Images/Icons/check-circle.svg';
 import xCircleIcon from '../../../Assets/Images/Icons/x-circle.svg';
 
-export function ToastMessage({ message, type }) {
+export function ToastMessage({ text, type }) {
   return (
     <Container type={type}>
       {type === 'success' && <img src={checkCircleIcon} alt="Success icon" />}
       {type === 'error' && <img src={xCircleIcon} alt="Error icon" />}
-      <strong>{message}</strong>
+      <strong>{text}</strong>
     </Container>
   );
 }
 
 ToastMessage.propTypes = {
-  message: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
   type: PropTypes.oneOf(['default', 'success', 'error']),
 };
 
